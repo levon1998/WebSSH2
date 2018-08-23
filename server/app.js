cf283e21aa8a7a3baa7a0f1fac5118f50bcd18d2
levon1998 <levon.babayan@fluxtechs.com>
2018-08-23 16:52:13 +0400

    open and connect to ssh bu server id

--- server/app.js
+++ server/app.js
@@ -17,13 +17,23 @@ var session = require('express-session')({
   unset: 'destroy'
 })
 var app = express()
+const mysql = require('mysql')
 var compression = require('compression')
 var server = require('http').Server(app)
 var myutil = require('./util')
 var validator = require('validator')
-var io = require('socket.io')(server, { serveClient: false })
+var io = require('socket.io')(server, {serveClient: false})
 var socket = require('./socket')
 var expressOptions = require('./expressOptions')
+
+const mysqlConnection = mysql.createConnection({
+  host: config.mysql.host,
+  user: config.mysql.user,
+  password: config.mysql.password,
+  database: config.mysql.database
+})
+
+mysqlConnection.connect()
 
 // express
 app.use(compression({level: 9}))
@@ -40,14 +50,24 @@ app.get('/reauth', function (req, res, next) {
   res.status(401).send('<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0; url=' + r + '"></head><body bgcolor="#000"></body></html>')
 })
 
-app.get('/ssh/host/:host?', function (req, res, next) {
+app.get('/ssh/server/:serverId', async function (req, res, next) {
+  const serverId = (validator.isIP(req.params.serverId + '') && req.params.serverId) ||
+    (validator.isFQDN(req.params.serverId) && req.params.serverId) ||
+    (/^(([a-z]|[A-Z]|[0-9]|[!^(){}\-_~])+)?\w$/.test(req.params.serverId) &&
+      req.params.serverId) || config.ssh.serverId
+  const basicHost = config.ssh.basicHost
+
+  let rows
+  try {
+    rows = await mysqlQuery(`SELECT ssh_ip_address as sshIpAddress, ssh_port_number as sshPortNumber, ssh_username as sshUsername FROM servers WHERE id = ?`, [serverId])
+  } catch (error) {
+    console.error(error)
+  }
+
   res.sendFile(path.join(path.join(publicPath, 'client.htm')))
   // capture, assign, and validated variables
   req.session.ssh = {
-    host: (validator.isIP(req.params.host + '') && req.params.host) ||
-      (validator.isFQDN(req.params.host) && req.params.host) ||
-      (/^(([a-z]|[A-Z]|[0-9]|[!^(){}\-_~])+)?\w$/.test(req.params.host) &&
-      req.params.host) || config.ssh.host,
+    host: basicHost,
     port: (validator.isInt(req.query.port + '', {min: 1, max: 65535}) &&
       req.query.port) || config.ssh.port,
     header: {
@@ -61,8 +81,14 @@ app.get('/ssh/host/:host?', function (req, res, next) {
       req.query.sshterm) || config.ssh.term,
     terminal: {
       cursorBlink: (validator.isBoolean(req.query.cursorBlink + '') ? myutil.parseBool(req.query.cursorBlink) : config.terminal.cursorBlink),
-      scrollback: (validator.isInt(req.query.scrollback + '', {min: 1, max: 200000}) && req.query.scrollback) ? req.query.scrollback : config.terminal.scrollback,
-      tabStopWidth: (validator.isInt(req.query.tabStopWidth + '', {min: 1, max: 100}) && req.query.tabStopWidth) ? req.query.tabStopWidth : config.terminal.tabStopWidth,
+      scrollback: (validator.isInt(req.query.scrollback + '', {
+        min: 1,
+        max: 200000
+      }) && req.query.scrollback) ? req.query.scrollback : config.terminal.scrollback,
+      tabStopWidth: (validator.isInt(req.query.tabStopWidth + '', {
+        min: 1,
+        max: 100
+      }) && req.query.tabStopWidth) ? req.query.tabStopWidth : config.terminal.tabStopWidth,
       bellStyle: ((req.query.bellStyle) && (['sound', 'none'].indexOf(req.query.bellStyle) > -1)) ? req.query.bellStyle : config.terminal.bellStyle
     },
     allowreplay: config.options.challengeButton || (validator.isBoolean(req.headers.allowreplay + '') ? myutil.parseBool(req.headers.allowreplay) : false),
@@ -73,7 +99,8 @@ app.get('/ssh/host/:host?', function (req, res, next) {
       server: config.serverlog.server || false
     },
     readyTimeout: (validator.isInt(req.query.readyTimeout + '', {min: 1, max: 300000}) &&
-      req.query.readyTimeout) || config.ssh.readyTimeout
+      req.query.readyTimeout) || config.ssh.readyTimeout,
+    connectionParams: rows[0]
   }
   if (req.session.ssh.header.name) validator.escape(req.session.ssh.header.name)
   if (req.session.ssh.header.background) validator.escape(req.session.ssh.header.background)
@@ -81,7 +108,7 @@ app.get('/ssh/host/:host?', function (req, res, next) {
 
 // express error handling
 app.use(function (req, res, next) {
-  res.status(404).send("Sorry can't find that!")
+  res.status(404).send('Sorry can\'t find that!')
 })
 
 app.use(function (err, req, res, next) {
@@ -98,5 +125,17 @@ io.use(function (socket, next) {
 
 // bring up socket
 io.on('connection', socket)
+
+function mysqlQuery (sqlQuery, params) {
+  return new Promise(function (resolve, reject) {
+    mysqlConnection.query(sqlQuery, params, function (error, result) {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(result)
+      }
+    })
+  })
+}
 
 module.exports = {server: server, config: config}
